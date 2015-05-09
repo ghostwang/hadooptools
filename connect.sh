88cc2bd82d93f89f89a7bd9994a8b7d398b7b525
WANG Jiaqi <mine4ghost@gmail.com>
2015-05-10 03:56:40 +0800

    change the var name

--- connect.sh
+++ connect.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-filestat=$( ping -c3 192.168.56.101|grep transmitted|awk '{print $4}')
-if((filestat==0));then
+vmstat=$( ping -c3 192.168.56.101|grep transmitted|awk '{print $4}')
+if((vmstat==0));then
     echo "connect time out"
 else
     ssh ghost@192.168.56.101 
